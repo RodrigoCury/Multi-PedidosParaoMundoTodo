724c6dbc6701228105ce36cb044294aa332fd580
= <=>
2021-10-09 18:19:37 -0300

    Base Spring App Created

---
+++ src/test/java/br/dev/rodrigocury/mudi/MudiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.rodrigocury.mudi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
