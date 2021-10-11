77ed99203bd953117e4a921765cbfc14e0c2c2d3
= <=>
2021-10-11 20:17:50 -0300

    Accepts Query to filter Results

--- src/main/java/br/dev/rodrigocury/mudi/controller/HomeController.java
+++ src/main/java/br/dev/rodrigocury/mudi/controller/HomeController.java
@@ -1,11 +1,13 @@
 package br.dev.rodrigocury.mudi.controller;
 
 import br.dev.rodrigocury.mudi.model.Pedido;
+import br.dev.rodrigocury.mudi.model.StatusPedido;
 import br.dev.rodrigocury.mudi.repository.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -16,8 +18,25 @@ public class HomeController {
     PedidoRepository pedidoRepository;
 
     @GetMapping("/")
-    public String home(Model model){
-        List<Pedido> pedidos = (List<Pedido>) pedidoRepository.findAll();
+    public String home(Model model, @RequestParam(name = "query", required = false) String query){
+        List<Pedido> pedidos = null;
+
+        switch (query != null ? query : ""){
+            case "waiting" -> {
+                pedidos = (List<Pedido>) pedidoRepository.findByStatus(StatusPedido.AGUARDANDO);
+            }
+            case "approved" -> {
+                pedidos = (List<Pedido>) pedidoRepository.findByStatus(StatusPedido.APROVADO);
+            }
+            case "delivered" -> {
+                pedidos = (List<Pedido>) pedidoRepository.findByStatus(StatusPedido.ENTREGUE);
+            }
+            default -> {
+                pedidos = (List<Pedido>) pedidoRepository.findAll();
+            }
+        }
+
+
 
         model.addAttribute("pedidos", pedidos);
         return "pedido/home";
