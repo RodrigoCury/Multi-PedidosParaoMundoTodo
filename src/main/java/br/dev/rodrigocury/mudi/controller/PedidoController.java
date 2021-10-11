7ab74483de6a636e0bef45f99778fd4a12f3535e
= <=>
2021-10-11 15:16:17 -0300

    Crud Complete

--- src/main/java/br/dev/rodrigocury/mudi/controller/PedidoController.java
+++ src/main/java/br/dev/rodrigocury/mudi/controller/PedidoController.java
@@ -5,10 +5,13 @@ import br.dev.rodrigocury.mudi.repository.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
+@RequestMapping("/api")
 public class PedidoController {
 
     @Autowired
@@ -19,4 +22,33 @@ public class PedidoController {
         pedidoRepository.save(pedido);
         return ResponseEntity.ok("OK");
     }
+
+    @GetMapping("/pedidos")
+    public ResponseEntity respondePedidos(){
+        List<Pedido> pedidos = (List<Pedido>) pedidoRepository.findAll();
+        return ResponseEntity.ok(pedidos);
+    }
+
+    @DeleteMapping("/pedidos/{id}")
+    public ResponseEntity deletaProduto(@PathVariable("id") Integer id){
+        Pedido pedidoADeletar = new Pedido();
+        pedidoADeletar.setId(id);
+        pedidoRepository.delete(pedidoADeletar);
+        return ResponseEntity.ok("OK");
+    }
+
+    @PutMapping("/pedidos/{id}")
+    public ResponseEntity atualizaProduto(@PathVariable("id") Integer id, @RequestBody Pedido pedido){
+        Optional<Pedido> pedidoNoDB = pedidoRepository.findById(id);
+        if(pedidoNoDB.isPresent()){
+            pedido.setId(id);
+            pedidoRepository.save(pedido);
+            return ResponseEntity.ok("Alterado");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+
 }
