a7897f0e353322a7b3b8ac15e79a28cf68e46284
= <=>
2021-10-13 15:23:13 -0300

    User's Pedidos Page

---
+++ src/main/java/br/dev/rodrigocury/mudi/controller/UsuarioController.java
@@ -0,0 +1,48 @@
+package br.dev.rodrigocury.mudi.controller;
+
+import br.dev.rodrigocury.mudi.model.Pedido;
+import br.dev.rodrigocury.mudi.model.StatusPedido;
+import br.dev.rodrigocury.mudi.model.User;
+import br.dev.rodrigocury.mudi.repository.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+import java.util.List;
+
+@Controller
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+    private PedidoRepository pedidoRepository;
+
+    @Autowired
+    public void setPedidoRepository(PedidoRepository pedidoRepository){
+        this.pedidoRepository = pedidoRepository;
+    }
+
+    @GetMapping("/pedidos")
+    public String home(Model model, Principal principal){
+        model.addAttribute("pedidos", pedidoRepository.findAllByUser(new User(principal.getName())));
+        return "usuario/home";
+    }
+
+    @GetMapping("pedidos/{status}")
+    public String filtrarPorStatus(@PathVariable(value = "status", required = true) String status, Model model, Principal principal){
+        List<Pedido> pedidos = pedidoRepository.findAllByUserAndStatus(new User(principal.getName()), StatusPedido.valueOf(status.toUpperCase()));
+        model.addAttribute("status", status.toLowerCase());
+        model.addAttribute("pedidos", pedidos);
+        return "usuario/home";
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String onIllegalArgumentExceptions(){
+        return "redirect:/";
+    }
+
+}
