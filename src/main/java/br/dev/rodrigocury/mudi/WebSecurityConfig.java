c921eba68cf57d422d5281913e9585bfb26a6453
= <=>
2021-10-13 15:23:26 -0300

    Updated Redirect on Login Success

--- src/main/java/br/dev/rodrigocury/mudi/WebSecurityConfig.java
+++ src/main/java/br/dev/rodrigocury/mudi/WebSecurityConfig.java
@@ -29,7 +29,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/", true)
+                .defaultSuccessUrl("/usuario/pedidos", true)
                 .permitAll()
                 .and()
                 .logout(httpSecurityLogoutConfigurer -> httpSecurityLogoutConfigurer.logoutUrl("/logout").permitAll())
