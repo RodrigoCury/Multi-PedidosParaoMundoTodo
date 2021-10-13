181ce593191dbb84ba567e5e6970718e0029b8b1
= <=>
2021-10-13 15:46:45 -0300

    Home is visible to all

--- src/main/java/br/dev/rodrigocury/mudi/WebSecurityConfig.java
+++ src/main/java/br/dev/rodrigocury/mudi/WebSecurityConfig.java
@@ -24,7 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/resources/**").permitAll()
+                .antMatchers("/resources/**", "/").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
